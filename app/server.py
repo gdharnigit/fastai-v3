8441acf09f043f2cc0639924a9c71d36b38efca0
gdharnigit <68172245+gdharnigit@users.noreply.github.com>
2020-07-12 20:27:01 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=11wbhWUDvvuYIhRGgC-auQozczC_CojIi'
-export_file_name = 'RS50stage-2.pth'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1liAaoaUOhBpmnevywYhZC04PGV0UNThT'
+export_file_name = 'res50_unet_8th_ind.pkl'
 
 classes = ['cats', 'dogs']
 path = Path(__file__).parent
